b79c33415f33666501d4d4fa2d67fb203398005e
LucasPort <lb.lucasbourdon@gmail.com>
2025-01-03 10:55:46 +0100

    appelle du json pour le mettre dans le front

--- src/views/cars.php
+++ src/views/cars.php
@@ -1,3 +1,8 @@
+<?php
+// Données JSON des véhicules
+$categorieVoitures = json_decode(file_get_contents(__DIR__ . '/../data/cars.json'), true);
+?>
+
 <main>
     <div class="h-full flex justify-end p-5">
         <select name="filtrer" id="filtrer" class="border-2 border-black">
@@ -9,78 +14,21 @@
     </div>
 
     <section class="bg-gray-100 h-fit p-5 grid md:grid-cols-4 gap-4 sm:grid-cols-3 max-sm:grid-cols-1 ">
-        <?php
-        include(dirname(__FILE__) . '../utils/process.php');
-
-        ?>
-        <div class="bg-blue-200 max-w-64 text-white text-left h-80 relative bg-cover-cars flex self-center">
-            <div
-                class="absolute  w-full h-full align-text-bottom left-0 cursor-pointer  hover:bg-gradient-to-b hover:to-black hover:from-transparent bottom-0">
-                <div class="bottom-0 absolute p-3 text-white">
-                    <h3>nom</h3>
-                    <p>portes</p>
-                    <p>engine</p>
-                    <p>pris</p>
-                </div>
+        <?php foreach ($categorieVoitures as $categorie => $voitures): ?>
+        <?php foreach ($voitures as $voiture): ?>
+        <div class="bg-blue-200 max-w-64 text-white text-left h-80 relative bg-cover-cars flex self-center"">
+        <div
+                class=" absolute w-full h-full align-text-bottom left-0 cursor-pointer hover:bg-gradient-to-b
+            hover:to-black hover:from-transparent bottom-0">
+            <div class="bottom-0 absolute p-3 text-white">
+                <h3><?= $voiture['nom'] ?></h3>
+                <p>portes</p>
+                <p>engine</p>
+                <p><?= $voiture['prix'] ?>€ /jour</p>
             </div>
         </div>
-        <div class="bg-blue-200 max-w-64 text-white text-left h-80 relative bg-cover-cars flex self-center">
-            <div
-                class="absolute  w-full h-full align-text-bottom left-0 cursor-pointer  hover:bg-gradient-to-b hover:to-black hover:from-transparent bottom-0">
-                <div class="bottom-0 absolute p-3 text-white">
-                    <h3>nom</h3>
-                    <p>portes</p>
-                    <p>engine</p>
-                    <p>pris</p>
-                </div>
-            </div>
         </div>
-        <div class="bg-blue-200 max-w-64 text-white text-left h-80 relative bg-cover-cars flex self-center">
-            <div
-                class="absolute  w-full h-full align-text-bottom left-0 cursor-pointer  hover:bg-gradient-to-b hover:to-black hover:from-transparent bottom-0">
-                <div class="bottom-0 absolute p-3 text-white">
-                    <h3>nom</h3>
-                    <p>portes</p>
-                    <p>engine</p>
-                    <p>pris</p>
-                </div>
-            </div>
-        </div>
-        <div class="bg-blue-200 max-w-64 text-white text-left h-80 relative bg-cover-cars flex self-center">
-            <div
-                class="absolute  w-full h-full align-text-bottom left-0 cursor-pointer  hover:bg-gradient-to-b hover:to-black hover:from-transparent bottom-0">
-                <div class="bottom-0 absolute p-3 text-white">
-                    <h3>nom</h3>
-                    <p>portes</p>
-                    <p>engine</p>
-                    <p>pris</p>
-                </div>
-            </div>
-        </div>
-        <div class="bg-blue-200 max-w-64 text-white text-left h-80 relative bg-cover-cars flex self-center">
-            <div
-                class="absolute  w-full h-full align-text-bottom left-0 cursor-pointer  hover:bg-gradient-to-b hover:to-black hover:from-transparent bottom-0">
-                <div class="bottom-0 absolute p-3 text-white">
-                    <h3>nom</h3>
-                    <p>portes</p>
-                    <p>engine</p>
-                    <p>pris</p>
-                </div>
-            </div>
-        </div>
-        <div class="bg-blue-200 max-w-64 text-white text-left h-80 relative bg-cover-cars flex self-center">
-            <div
-                class="absolute  w-full h-full align-text-bottom left-0 cursor-pointer  hover:bg-gradient-to-b hover:to-black hover:from-transparent bottom-0">
-                <div class="bottom-0 absolute p-3 text-white">
-                    <h3>nom</h3>
-                    <p>portes</p>
-                    <p>engine</p>
-                    <p>pris</p>
-                </div>
-            </div>
-        </div>
-
+        <?php endforeach; ?>
+        <?php endforeach; ?>
     </section>
-
-
 </main>
