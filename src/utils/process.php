d8b021561b3eb2d2d21f386f3a5eade539cb1413
LucasPort <lb.lucasbourdon@gmail.com>
2025-01-03 10:55:15 +0100

    update du process

--- src/utils/process.php
+++ src/utils/process.php
@@ -9,128 +9,8 @@ var_dump($data);
  *      - energie_du_moteur : l'énergie du moteur du véhicule
  *      - prix : le prix du véhicule à la journée
  */
-$categorieVoitures = [
-    'berline' => [
-        [
-            'nom' => 'BMW Série 3',
-            'nombre_de_portes' => 4,
-            'energie_du_moteur' => 'Essence',
-            'prix' => 45,
-        ],
-        [
-            'nom' => 'Mercedes Classe C',
-            'nombre_de_portes' => 4,
-            'energie_du_moteur' => 'Diesel',
-            'prix' => 48,
-        ],
-        [
-            'nom' => 'Audi A4',
-            'nombre_de_portes' => 4,
-            'energie_du_moteur' => 'Hybride',
-            'prix' => 50,
-        ],
-    ],
-    'camion' => [
-        [
-            'nom' => 'Volvo FH16',
-            'nombre_de_portes' => 2,
-            'energie_du_moteur' => 'Diesel',
-            'prix' => 120,
-        ],
-        [
-            'nom' => 'Scania R450',
-            'nombre_de_portes' => 2,
-            'energie_du_moteur' => 'Diesel',
-            'prix' => 110,
-        ],
-        [
-            'nom' => 'Mercedes-Benz Actros',
-            'nombre_de_portes' => 2,
-            'energie_du_moteur' => 'Diesel',
-            'prix' => 115,
-        ],
-    ],
-    'citadine' => [
-        [
-            'nom' => 'Renault Clio',
-            'nombre_de_portes' => 5,
-            'energie_du_moteur' => 'Essence',
-            'prix' => 17,
-        ],
-        [
-            'nom' => 'Peugeot 208',
-            'nombre_de_portes' => 5,
-            'energie_du_moteur' => 'Diesel',
-            'prix' => 18,
-        ],
-        [
-            'nom' => 'Volkswagen Polo',
-            'nombre_de_portes' => 5,
-            'energie_du_moteur' => 'Hybride',
-            'prix' => 20,
-        ],
-    ],
-    'sans-permis' => [
-        [
-            'nom' => 'Aixam City',
-            'nombre_de_portes' => 2,
-            'energie_du_moteur' => 'Électrique',
-            'prix' => 14,
-        ],
-        [
-            'nom' => 'Ligier JS50',
-            'nombre_de_portes' => 2,
-            'energie_du_moteur' => 'Diesel',
-            'prix' => 15,
-        ],
-        [
-            'nom' => 'Microcar Dué',
-            'nombre_de_portes' => 2,
-            'energie_du_moteur' => 'Essence',
-            'prix' => 13,
-        ],
-    ],
-    'sportive' => [
-        [
-            'nom' => 'Porsche 911',
-            'nombre_de_portes' => 2,
-            'energie_du_moteur' => 'Essence',
-            'prix' => 120,
-        ],
-        [
-            'nom' => 'Ferrari Roma',
-            'nombre_de_portes' => 2,
-            'energie_du_moteur' => 'Essence',
-            'prix' => 200,
-        ],
-        [
-            'nom' => 'Lamborghini Huracán',
-            'nombre_de_portes' => 2,
-            'energie_du_moteur' => 'Essence',
-            'prix' => 220,
-        ],
-    ],
-    'suv' => [
-        [
-            'nom' => 'Toyota RAV4',
-            'nombre_de_portes' => 5,
-            'energie_du_moteur' => 'Hybride',
-            'prix' => 35,
-        ],
-        [
-            'nom' => 'BMW X5',
-            'nombre_de_portes' => 5,
-            'energie_du_moteur' => 'Diesel',
-            'prix' => 70,
-        ],
-        [
-            'nom' => 'Audi Q7',
-            'nombre_de_portes' => 5,
-            'energie_du_moteur' => 'Essence',
-            'prix' => 80,
-        ],
-    ],
-];
+
+$categorieVoitures = json_decode(file_get_contents(__DIR__ . '/../data/cars.json'), true);
 
 /**
  * Fonction qui retourne les véhicules correspondant au type
@@ -154,13 +34,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         'telephone' => $_POST['telephone'] ?? '',
     ];
 
+    if (!empty($data['vehicle_type']) && in_array($data['vehicle_type'], array_keys($categorieVoitures))) {
+        $data['vehicles'] = $categorieVoitures[$data['vehicle_type']] ?? '';
+    }
+
     foreach ($_POST as $key => $value) {
         if (!in_array($key, array_keys($data))) {
             $data['type'] = $key;
             $data['vehicles'] = $categorieVoitures[$key] ?? '';
         }
     }
-
 
     // Préparation du mail
 
